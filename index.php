f1dffd0d9f3caf149f4ea6a14a4a847aa7856aa0
asifbhuiyann <getconnectwithasif@gmail.com>
2023-10-02 20:21:22 +0600

    This is the homepage file. Whenever someone search for this browser will take here to this file.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Welcome to Our Website</title>
+</head>
+
+<body>
+<?php include 'navbar.php'; ?>
+    <section class="text-center mt-5 mb-5">
+        <h2>Hello, Guest!</h2>
+        <p>We're glad you're here. Discover amazing content and more.</p>
+    </section>
+    <?php include 'footer.php'; ?>
+    
+</body>
+
+</html>
