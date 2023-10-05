06edf9d0db1c3e4ff5b821c274b614e079d31465
asifbhuiyann <getconnectwithasif@gmail.com>
2023-10-05 22:47:00 +0600

    About us php file added

---
+++ about.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></head>
+
+<body>
+<?php include 'navbar.php'; ?>
+    <div class="container mt-5 mb-5">
+        <h1 class ="mb-5">About Price Tracking System</h1>
+        <p class ="mb-5">
+            In today's rapidly evolving technology landscape, staying updated with the latest prices of IT products is
+            essential for making informed purchasing decisions. Our cutting-edge IT Product Price Tracker is your
+            ultimate solution for effortless price monitoring across a wide range of IT gadgets, devices, and
+            components. <br><br>
+
+            <strong> What It Does:</strong><br>
+
+            Our IT Product Price Tracker is a powerful tool designed to streamline your shopping experience by providing
+            real-time price tracking and comparison for a diverse array of IT products. From laptops and smartphones to
+            graphics cards and peripherals, our platform covers it all. <br><br>
+            
+            <strong>Here's how it works:</strong><br>
+            
+            Comprehensive Tracking: Our platform scans multiple online retailers, marketplaces, and e-commerce websites
+            to gather up-to-the-minute pricing data for the products you're interested in. <br><br>
+
+            <strong> Price History Insights:</strong><br>
+            Gain valuable insights into the historical price trends of your desired products.
+            Visualize price fluctuations over time to identify the best times to make your purchases and save money. <br><br>
+
+            <strong> Instant Alerts:</strong><br>
+            Set price thresholds for products, and receive instant notifications via email or mobile app
+            when the price drops to your desired level. Never miss out on a deal again.<br><br>
+
+            <strong>Comparison Made Easy:</strong> <br>
+            Easily compare prices across different retailers to ensure you're getting the best
+            deal available. Make an informed decision by assessing the price variations and offers from various sources.<br><br>
+
+            <strong>User-Friendly Interface:</strong> <br>
+            Our intuitive and user-friendly interface makes navigating the platform a breeze.
+            Effortlessly add products, track prices, and manage your alerts all from one centralized dashboard.<br><br>
+
+            <strong>Why It's Convenient:</strong> <br><br>
+
+            <strong> Time-Saving:</strong> <br>
+            Say goodbye to manually checking prices across numerous websites. Our tracker aggregates all
+            the data you need, saving you time and effort.<br><br>
+
+            <strong>Cost-Efficient:</strong> <br>
+            Make purchases at the right time and at the best price, ensuring you get the most value for
+            your money.<br><br>
+
+            <strong>Smart Shopping:</strong> <br>
+            With historical price data and instant alerts, you'll be equipped to make well-informed
+            decisions, avoiding buyer's remorse.<br><br>
+
+            <strong>Flexibility: </strong><br>
+            Whether you're a tech enthusiast, a professional, or simply a smart shopper, our platform
+            caters to all levels of IT product consumers.<br><br>
+
+            <strong> Stay Ahead of Deals: </strong> <br>
+            Be the first to know about price drops, limited-time offers, and exclusive deals,
+            giving you the upper hand in the fast-paced world of technology sales.<br><br>
+
+        </p>
+    </div>
+
+    <?php include 'footer.php'; ?>
+    
+</body>
+
+</html>
